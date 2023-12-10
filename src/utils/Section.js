d8a850b5afd5b1b8be162a79cc81e5bd07bffc35
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2023-12-10 16:59:50 +0100

    ARREGLAR SECTION.JS
    sadkasdjsakdhsakdhsadsadasadasdsa

--- src/utils/Section.js
+++ src/utils/Section.js
@@ -1,11 +1,11 @@
 import Btn from "./Btn";
 
 function Section({ h, p, i, ih, b }) {
-    const renderImage = i !== null ? <img src={i}/> : null;
+    const renderImage = i !== null ? <img className="w-64 object-scale-down h-auto" src={i}/> : null;
     const renderBtn = (ih !== null && b !== null) ? <Btn text={b} href={ih} /> : null;
 
     return (
-        <section className="flex bg-white flex-col h-screen w-auto mx-auto items-start justify-center gap-4">
+        <section className="flex bg-white flex-row h-screen w-auto mx-auto items-start justify-center gap-4">
             {h && <h1 className="font-mono text-xl text-black">{h}</h1>}
             {p && <h3 className="font-sans text-black">{p}</h3>}
             {renderImage}
