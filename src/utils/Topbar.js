6bbfce0b3cbc2b6b7f486a9bbddc7b3bc8a34b61
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2023-12-10 13:46:09 +0100

    commit inicial (ma o meno)
    es de prueba

---
+++ src/utils/Topbar.js
@@ -0,0 +1,9 @@
+function Topbar() {
+    return (
+        <nav className="flex flex-row gap-4 p-4 bg-slate-900 ">
+            <h1 className="font-mono">PersonaPlus</h1>
+        </nav>
+    )
+}
+
+export default Topbar;
