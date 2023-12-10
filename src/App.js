656ff2a3e075b793cc90bca54473c3ccdf6b9d29
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2023-12-10 16:51:45 +0100

    bery interesting update
    XD

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import Topbar from './utils/Topbar.js';
 import Hero from './utils/Hero.js';
 import React from 'react';
 import Btn from './utils/Btn.js';
+import Section from './utils/Section.js';
 
 function App() {
   return (
     <main className="dark w-screen overflow-x-hidden">
       <Topbar></Topbar>
       <Hero image="/hero.png"></Hero>
-      <section className="flex bg-white flex-col h-screen w-auto mx-auto items-center justify-center gap-4">
-        <h1 className="font-sans text-black">PersonaPlus es la app de salud que te acompaña y te ayuda a darle el PLUS que necesitas a como te cuidas.</h1>
-        <h3 className="font-mono text-black">PersonaPlus aún está en desarrollo. ¡Pronto estará disponible!</h3>
-        <Btn text='Mientras tanto, ¿y si nos sigues en YouTube?' href='https://youtube.com/@ZakaHaceCosas'>
-        </Btn>
-      </section>
+      <Section
+        h='¿Qué es PersonaPlus?'
+        p='PersonaPlus es una aplicación de salud y antivirus 2 en 1, para cuidar de tu bienestar (tanto físico como digital) y para proteger tu teléfono y mantenerlo limpio.'
+        i='/hero.png'
+        b={null}
+        ih={null}
+      >
+      </Section>
+      <Section
+        h='¿Donde lo descargo?'
+        p='PersonaPlus aún no está disponible, está todavía siendo desarrollado.'
+        i={null}
+        b='Puedes ver el código en GitHub'
+        ih='https://github.com/ZakaHaceCosas/personaplus'
+      >
+      </Section>
     </main>
   );
 }
--- src/utils/Btn.js
+++ src/utils/Btn.js
@@ -1,6 +1,6 @@
 function Btn({ text, href }) {
     return (
-        <a className="p-4 text-center font-sans cursor-pointer bg-green-800 text-white hover:bg-green-400 hover:text-black" href={href}>
+        <a className="rounded-md transition-all duration-300 p-4 text-center font-sans cursor-pointer bg-green-800 text-white hover:bg-green-400 hover:text-black" href={href}>
             {text}
         </a>
     );
