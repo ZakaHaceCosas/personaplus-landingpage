9ac0d5acf0dd13730f2bc50d06387ffff81b5512
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2023-12-10 14:32:43 +0100

    EL SRC ASDSAJDHASKDHKSAJDHASKD
    ARREGLATE!"!!"!"!uidsyhdasuidsaddsad POFRAOFPAR

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <main className="dark w-screen overflow-x-hidden">
       <Topbar></Topbar>
-      <Hero image="../public/hero.png"></Hero>
+      <Hero image="/hero.png"></Hero>
       <section className="flex bg-white flex-col h-screen w-auto mx-auto items-center justify-center gap-4">
         <h3 className="font-mono text-black">¡Pronto disponible!</h3>
         <Btn text='Síguenos en YouTube para estar al tanto de lo que hacemos' href='https://youtube.com/@ZakaHaceCosas'>
