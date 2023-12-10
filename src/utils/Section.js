4313f0fc7200d9678d2142cb1ce67b2bc35d5bfd
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2023-12-10 17:02:45 +0100

    Update Section.js

--- src/utils/Section.js
+++ src/utils/Section.js
@@ -1,15 +1,17 @@
 import Btn from "./Btn";
 
 function Section({ h, p, i, ih, b }) {
-    const renderImage = i !== null ? <img className="w-64 object-scale-down h-auto" src={i}/> : null;
+    const renderImage = i !== null ? <img className="w-72 object-scale-down h-auto" src={i}/> : null;
     const renderBtn = (ih !== null && b !== null) ? <Btn text={b} href={ih} /> : null;
 
     return (
         <section className="flex bg-white flex-row h-screen w-auto mx-auto items-start justify-center gap-4">
-            {h && <h1 className="font-mono text-xl text-black">{h}</h1>}
-            {p && <h3 className="font-sans text-black">{p}</h3>}
             {renderImage}
-            {renderBtn}
+            <div className="flex flex-column items-start justify-items-start gap-2">
+                {h && <h1 className="font-mono text-xl text-black">{h}</h1>}
+                {p && <h3 className="font-sans text-black">{p}</h3>}
+                {renderBtn}
+            </div>
         </section>
     );
 }
