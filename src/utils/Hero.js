f3cd54fdc5b58c5e053092fb1a88e58833538ca1
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2023-12-10 14:34:56 +0100

    Update Hero.js

--- src/utils/Hero.js
+++ src/utils/Hero.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Hero({ image }) {
     return (
-        <section id="hero" className="bg-emerald-100 flex h-screen w-screen flex-row items-center justify-around">
-            <img src={image}></img>
+        <section id="hero" className="bg-emerald-100 flex h-screen flex-row items-center justify-around">
+            <img src={image} className="w-52 h-auto object-scale-down"></img>
             <h1 className="font-mono text-black text-lg text-center">Dale un<br></br><span className="font-bold text-9xl text-green-400">PLUS</span><br></br>a tu persona</h1>
         </section>
     )
