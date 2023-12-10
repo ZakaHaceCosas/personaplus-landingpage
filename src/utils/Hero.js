73a85db6ebe776e433cc1128ffb4ae4196315906
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2023-12-10 14:20:22 +0100

    añadir el hero (por tercera vez)
    a ver si ahora furula la ctm

--- src/utils/Hero.js
+++ src/utils/Hero.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Hero() {
+function Hero({ image }) {
     return (
         <section id="hero" className="bg-emerald-100 flex h-screen w-screen flex-row items-center justify-around">
-            <img src="./hero.png"></img>
-            <h1 className="font-mono text-white text-lg">Dale un<br></br><span className="font-bold text-9xl text-green-400">PLUS</span><br></br>a tu persona</h1>
+            <img src={image}></img>
+            <h1 className="font-mono text-black text-lg text-center">Dale un<br></br><span className="font-bold text-9xl text-green-400">PLUS</span><br></br>a tu persona</h1>
         </section>
     )
 }
