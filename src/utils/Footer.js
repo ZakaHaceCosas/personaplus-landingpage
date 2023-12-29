f5e6612f191774d26433500dfac36ed906bc7baa
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2023-12-29 16:01:15 +0100

    style foter

--- src/utils/Footer.js
+++ src/utils/Footer.js
@@ -7,7 +7,7 @@ function Footer({ image }) {
                 <img src='/favicon.svg' alt="Logotipo de PersonaPlus"></img>
                 <p>Dale un PLUS a tu Persona</p>
             </div>
-            <div className='fsect'>
+            <div className='fsect' style='width: 30%'>
                 <h3>PersonaPlus</h3>
                 <p>Hecho por <a href='https://github.com/ZakaHaceCosas'>@ZakaHaceCosas</a></p>
                 <img src='/Z.LogoFull.White.svg' alt="Logo de ZakaHaceCosas"></img>
