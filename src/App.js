3bd13d2c45c307c10a0e2241b6e46b4d2baad7d7
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-01-05 15:01:54 +0100

    limpieza del app.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React, { useState } from 'react';
 import Home from './utils/Home';
-//import Docs from './utils/Docs';
 import './utils/App.css'
 import Footer from './utils/Footer';
 
 function App() {
-  /*
-  const [currentPage, setCurrentPage] = useState('home');
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'home':
-        return <Home />;
-      case 'docs':
-        return <Docs />;
-      default:
-        return <Home />;
-    }
-  };
-  */
   return (
     <>
       <nav>
         <h1>PersonaPlus</h1>
-        {/*
-        <a onClick={() => setCurrentPage('home')} >
-          Inicio
-        </a>
-        <a onClick={() => setCurrentPage('home')}>
-          Documentación
-        </a>
-        */}
         <a href="https://personaplus.vercel.app">
           Inicio
         </a>
@@ -38,13 +15,8 @@ function App() {
           Documentación
         </a>
       </nav>
-
-      {/*renderPage()*/}
-      
       <Home></Home>
-
       <Footer></Footer>
-
     </>
   );
 }
