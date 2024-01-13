099894abde51a799d9845aa25e33f3b4d42e195a
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-01-13 17:13:24 +0100

    Update Footer.js
    a ver so esto lo arregla

--- src/utils/Footer.js
+++ src/utils/Footer.js
@@ -3,11 +3,11 @@ import React from 'react';
 function Footer({ image }) {
     return (
         <footer>
-            <div className='fsect'>
+            <div className='fsect' style={{ width: '30%' }}>
                 <img src='/favicon.svg' alt="Logotipo de PersonaPlus"></img>
                 <p>Dale un PLUS a tu Persona</p>
             </div>
-            <div className='fsect' style='width: 30%'>
+            <div className='fsect' style={{ width: '30%' }}>
                 <h3>PersonaPlus</h3>
                 <p>Hecho por <a href='https://github.com/ZakaHaceCosas'>@ZakaHaceCosas</a></p>
                 <img src='/Z.LogoFull.White.svg' alt="Logo de ZakaHaceCosas"></img>
