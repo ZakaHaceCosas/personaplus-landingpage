897f6dbb8c61bde6b7393a375b58f1d6063be5d6
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-02-16 16:33:55 +0100

    Update Footer.js

--- src/utils/Footer.js
+++ src/utils/Footer.js
@@ -3,14 +3,14 @@ import React from 'react';
 function Footer({ image }) {
     return (
         <footer>
-            <div className='fsect' style={{ width: '30%' }}>
-                <img src='/favicon.svg' alt="Logotipo de PersonaPlus"></img>
+            <div className='fsect'>
+                <img src='/favicon.svg' alt="Logotipo de PersonaPlus" class="pplogo"></img>
                 <p>Dale un PLUS a tu Persona</p>
             </div>
-            <div className='fsect' style={{ width: '30%' }}>
-                <h3>PersonaPlus</h3>
+            <div className='fsect'>
+                <h3 class="jeth">PersonaPlus</h3>
                 <p>Hecho por <a href='https://github.com/ZakaHaceCosas'>@ZakaHaceCosas</a></p>
-                <img src='/Z.LogoFull.White.svg' alt="Logo de ZakaHaceCosas"></img>
+                <img src='/Z.LogoFull.White.svg' alt="Logo de ZakaHaceCosas" class="zlogo"></img>
             </div>
         </footer>
     )
