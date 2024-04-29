81f5ed7c5aba6ab3f52532c251bede348b8214b4
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-04-29 23:16:05 +0200

    preparativos

--- src/utils/Footer.js
+++ src/utils/Footer.js
@@ -4,13 +4,17 @@ function Footer({ image }) {
     return (
         <footer>
             <div className='fsect'>
+                <h1 class="jeth">PersonaPlus</h1>
                 <img src='/favicon.svg' alt="Logotipo de PersonaPlus" class="pplogo"></img>
                 <p>Dale un PLUS a tu Persona</p>
             </div>
             <div className='fsect'>
-                <h1 class="jeth">PersonaPlus</h1>
-                <p>Hecho por <a href='https://github.com/ZakaHaceCosas'>@ZakaHaceCosas</a></p>
                 <img src='/Z.LogoFull.White.svg' alt="Logo de ZakaHaceCosas" class="zlogo"></img>
+                <p>Hecho por <a href='https://github.com/ZakaHaceCosas'>@ZakaHaceCosas</a>.<br><b>Más de ZakaHaceCosas:</b></p>
+                <ul>
+                    <li><a href="https://youtube.com/@ZakaHaceCosas" target="_blank">Canal de YouTube</a></li>
+                    <li><a href="https://youtube.com/@ZakaProfe" target="_blank">ZakaProfe</a></li>
+                </ul>
             </div>
         </footer>
     )
