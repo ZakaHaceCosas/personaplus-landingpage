eb38a969c83336dd3546fe781374922a27975c67
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2024-05-01 23:15:51 +0200

    Update Footer.js

--- src/utils/Footer.js
+++ src/utils/Footer.js
@@ -9,12 +9,13 @@ function Footer({ image }) {
             </div>
             <div className='fsect'>
                 <img src='/zakahacecosas.png' alt="Logo de ZakaHaceCosas" className="zlogo" />
-                <p>Hecho por <a href='https://github.com/ZakaHaceCosas' rel="noopener noreferrer">@ZakaHaceCosas</a>.<br />
-                    <b>Más de ZakaHaceCosas:</b></p>
-                <ul>
-                    <li><a href="https://youtube.com/@ZakaHaceCosas" target="_blank" rel="noopener noreferrer">Canal de YouTube</a></li>
-                    <li><a href="https://youtube.com/@ZakaProfe" target="_blank" rel="noopener noreferrer">ZakaProfe</a></li>
-                </ul>
+                <p>Hecho por <a href='https://github.com/ZakaHaceCosas' rel="noopener noreferrer">@ZakaHaceCosas</a>.</p>
+                <hr />
+                <p><b>Más de ZakaHaceCosas:</b></p>
+                <div className="stuffactualrow">
+                    <a className="btn btnGran" href="https://youtube.com/@ZakaHaceCosas" target="_blank" rel="noopener noreferrer">Canal de YouTube</a>
+                    <a className="btn btnGran" href="https://youtube.com/@ZakaProfe" target="_blank" rel="noopener noreferrer">ZakaProfe</a>
+                </div>
             </div>
         </footer>
     )
