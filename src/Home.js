42b1b02041803a6919afd0925baaaf48e13e36dd
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-07-10 21:21:44 +0200

    update website

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,19 @@ function Home() {
     return (
         <>
             <main>
+                <div className="news">
+                    <p>
+                        {t("wevelauncherfirstdemo")}{" "}
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://github.com/ZakaHaceCosas/personaplus/releases/tag/0.0.1-R5-b20"
+                        >
+                            {t("here")}
+                        </a>
+                        !
+                    </p>
+                </div>
                 <Hero></Hero>
                 <div className="stuffrow" id="lookatit">
                     <h1>{t("lookatit")}</h1>
@@ -34,7 +47,9 @@ function Home() {
                             {t("whatispptextblock")}
                         </h3>
                     </div>
-                    <img src="/mockup1.png" alt="Decorative." />
+                    <div className="imagediv">
+                        <img src="/mockup1.png" alt="Decorative." />
+                    </div>
                 </section>
                 <section>
                     <div>
@@ -43,14 +58,27 @@ function Home() {
                             {t("howtouseblock")}
                         </h3>
                     </div>
-                    <img src="/mockup2.png" alt="Decorative." />
+                    <div className="imagediv">
+                        <img src="/mockup2.png" alt="Decorative." />
+                        <p
+                            style={{
+                                textWrap: "wrap",
+                                fontSize: "xx-small",
+                                opacity: 0.5,
+                            }}
+                        >
+                            {t("mockups")}
+                        </p>
+                    </div>
                 </section>
                 <section id="privacy">
-                    <img
-                        style={{ height: "60%" }}
-                        src={`/${t("differentimg")}.png`}
-                        alt="Decorative."
-                    />
+                    <div className="imagediv">
+                        <img
+                            style={{ height: "60%" }}
+                            src={`/${t("differentimg")}.png`}
+                            alt="Decorative."
+                        />
+                    </div>
                     <div>
                         <h1>{t("different")}</h1>
                         <p>{t("differentblock")}</p>
@@ -74,20 +102,6 @@ function Home() {
                         </div>
                     </div>
                 </section>
-                <section>
-                    <div>
-                        <h1 style={{ fontSize: "52px" }}>
-                            {t("wheredownload")}
-                        </h1>
-                        <h3 style={{ fontWeight: 400 }}>
-                            {t("wheredownloadblock")}{" "}
-                            <a href="#socials">YouTube</a>
-                            {t("wheredownloadblockconnector")}{" "}
-                            <a href="#socials">Discord</a>
-                            {t("wheredownloadblock2")}
-                        </h3>
-                    </div>
-                </section>
                 <div className="stuffrow" id="socials">
                     <h1>{t("gyap")}</h1>
                     <p
@@ -97,9 +111,17 @@ function Home() {
                             textAlign: "center",
                         }}
                     >
-                        {t("solodev")}
+                        {t("downloadnow")}
                     </p>
                     <div className="stuffactualrow">
+                        <a
+                            className="btn btnGran"
+                            href="https://github.com/ZakaHaceCosas/personaplus/releases/tag/0.0.1-R5-b20"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {t("downloadbtn")}
+                        </a>
                         <a
                             className="btn btnRot"
                             href="https://youtube.com/@ZakaHaceCosas"
@@ -117,7 +139,7 @@ function Home() {
                             {t("ghrepo")}
                         </a>
                         <a
-                            className="btn btnGran"
+                            className="btn btnBlau"
                             href="https://discord.gg/euVHrr46c6"
                             target="_blank"
                             rel="noopener noreferrer"
@@ -125,6 +147,17 @@ function Home() {
                             {t("disc")}
                         </a>
                     </div>
+                    <p
+                        style={{
+                            maxWidth: "50vw",
+                            textWrap: "wrap",
+                            textAlign: "center",
+                            fontSize: "xx-small",
+                            opacity: 0.5,
+                        }}
+                    >
+                        {t("downloaddisclaimer")}
+                    </p>
                 </div>
             </main>
         </>
