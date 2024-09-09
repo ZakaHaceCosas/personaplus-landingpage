6b886d3e80fc692527e6351221d654e763f946bc
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-09-09 09:55:44 +0200

    Remove StrictMode (production)

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ import "./utils/i18n";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+root.render(<App />);
 
 reportWebVitals(sendToVercelAnalytics);
